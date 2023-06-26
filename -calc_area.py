b47ca6e753428226b49751fd79a48693f8483d55
eMobilis <develop.tedd@gmail.com>
2023-06-26 09:13:48 +0300

    simple area of a rectangle program

---
+++ -calc_area.py
@@ -0,0 +1,10 @@
+#enable user to enter input
+height = input("Enter height")
+width = input("Enter Width")
+#convert two variables into integers
+height = int(height)
+width = int(width)
+#perfom calculation
+result = height * width
+
+print("Area is", result)
---
+++ months_calculator.py
@@ -0,0 +1,6 @@
+user_age = input("Enter Your Age")
+age = int(user_age)
+
+calc_months = age * 12
+
+print(calc_months)
